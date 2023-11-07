28af2de42ccbc1171189a470686a5f115f12411a
Logan Matheny <loganpaulmatheny@gmail.com>
2023-11-07 12:07:53 -0500

    Connect API and hide sensitive data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,25 +1,41 @@
-// Think about setting all these with defaults in case there's something missing
-export const cleanParkData = (parks) => {
+const apiKey = process.env.REACT_APP_API_KEY;
+
+export const cleanParksData = (parks) => {
   const parksData = parks.data;
-  const cleanParks = parksData.map((park) => {
-    let cleanPark = {
-      id: park.id,
-      fullName: park.fullName ?? "No Name",
-      url: park.url || "No website",
-      description: park.description || "No description available",
-      directionsUrl: park.directionsUrl || "No directions available",
-      address: {
-        street: park.addresses?.[0]?.line1 || "No street address",
-        city: park.addresses?.[0]?.city || "No city",
-        stateCode: park.addresses?.[0]?.stateCode || "NA",
-        zip: park.addresses?.[0]?.postalCode || "00000",
-      },
-      images: park.images || [],
-      weatherInfo: park.weatherInfo || "Weather information not available",
-    };
-    // console.log(cleanPark);
-    return cleanPark;
+
+  const cleanParks = parksData.filter((park) => {
+    if (park.designation === "National Park") {
+      let cleanPark = {
+        id: park.id,
+        fullName: park.fullName ?? "No Name",
+        url: park.url || "No website",
+        description: park.description || "No description available",
+        directionsUrl: park.directionsUrl || "No directions available",
+        address: {
+          street: park.addresses?.[0]?.line1 || "No street address",
+          city: park.addresses?.[0]?.city || "No city",
+          stateCode: park.addresses?.[0]?.stateCode || "NA",
+          zip: park.addresses?.[0]?.postalCode || "00000",
+        },
+        images: park.images || [],
+        weatherInfo: park.weatherInfo || "Weather information not available",
+      };
+      // console.log(cleanPark);
+      return cleanPark;
+    }
   });
+
   // console.log(cleanParks);
   return cleanParks;
+};
+
+export const getParks = () => {
+  return fetch(
+    `https://developer.nps.gov/api/v1/parks?limit=550&q=acadia&api_key=${apiKey}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 };
