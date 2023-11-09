cc0bf1dba82c824de45e07286266a088e34fdd4c
Logan Matheny <loganpaulmatheny@gmail.com>
2023-11-09 11:01:21 -0500

    Complete the error component

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,8 +1,28 @@
 import "./Error.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Error() {
-  return <div>Error</div>;
+  return (
+    <div className="error-container">
+      <h1 className="error-message">
+        Oops, looks like you might need to check the map
+      </h1>
+      <img
+        className="need-adventure"
+        src={process.env.PUBLIC_URL + "/map.png"}
+        alt=""
+      />
+      <Link to="/">
+        <img
+          className="back-button"
+          src={process.env.PUBLIC_URL + "/turn-around.png"}
+          alt=""
+        />
+      </Link>
+      <p>Back to Home</p>
+    </div>
+  );
 }
 
 export default Error;
