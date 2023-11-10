bbe347bd3d930a03837b7de0b404c953749a7c0b
Logan Matheny <loganpaulmatheny@gmail.com>
2023-11-09 19:52:05 -0500

    Fix reload problem

--- src/Components/ParkBlowUp/ParkBlowUp.js
+++ src/Components/ParkBlowUp/ParkBlowUp.js
@@ -1,68 +1,82 @@
 import "./ParkBlowUp.css";
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
 function ParkBlowUp({ park = {}, toggleVisit = () => {} }) {
-  const [error, setError] = useState("");
+  // Programatically navigate
+  const navigate = useNavigate();
+  // const [isInitialLoad, setIsInitialLoad] = useState(true);
 
+  // This will force a reload of home page and then immediately route to the blowup page!
   useEffect(() => {
-    if (!(park && park.images && park.images.length > 0)) {
-      setError("yup there's a problem");
-    }
-  }, [park]);
+    const handleBeforeUnload = (event) => {
+      // Is the page being reloaded
+      const isReloading = event.type === "beforeunload";
+      // The beforeunload event is just before you leave a page
+      if (isReloading) {
+        // !isInitialLoad ^ removed
+        console.log("redirect occured");
+        // Redirect to the home page
+        navigate("/");
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    // setIsInitialLoad(false);
+
+    // Cleanup function, prevent a memory link
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [navigate]);
 
   const images =
-    park && park.images && park.images.length > 0
-      ? park.images.map((image) => (
-          <img
-            key={image.url}
-            className="blow-up-image"
-            src={image.url}
-            alt={image.altText}
-          />
-        ))
-      : null;
+    park.images?.map((image) => (
+      //  Check and see if park is undefined before mapping over it and throwing an error
+      <img
+        key={image.url}
+        className="blow-up-image"
+        src={image.url}
+        alt={image.altText}
+      />
+    )) || [];
 
   return (
-    <div>
-      {error ? (
-        <h2>Oops</h2>
-      ) : (
-        <article className="park-info">
-          <div className="title">
-            <h1>{park.fullName}</h1>
-            {!park.visited ? (
-              <img
-                className="visit-icon-blowup"
-                src={process.env.PUBLIC_URL + "/toVisit.png"}
-                alt="an empty trail signpost"
-                onClick={() => toggleVisit(park.id)}
-              />
-            ) : (
-              <img
-                className="visit-icon-blowup"
-                src={process.env.PUBLIC_URL + "/visited.png"}
-                alt="a colored trail signpost"
-                onClick={() => toggleVisit(park.id)}
-              />
-            )}
-          </div>
-          <div className="park-image-container">{images}</div>
-          <div className="details-container">
-            <h2>Description:</h2>
-            <p>{park.description}</p>
-            <p>
-              <strong>Address: </strong>
-              {park.address && park.address.street}{" "}
-              {park.address && park.address.city},{" "}
-              {park.address && park.address.stateCode}{" "}
-              {park.address && park.address.zip}
-            </p>
-            <a href={park.directionsUrl}>Directions</a>
-          </div>
-        </article>
-      )}
-    </div>
+    <article className="park-info">
+      <div className="title">
+        <h1>{park.fullName}</h1>
+        {!park.visited ? (
+          <img
+            className="visit-icon-blowup"
+            src={process.env.PUBLIC_URL + "/toVisit.png"}
+            alt="an empty trail signpost"
+            onClick={() => toggleVisit(park.id)}
+          />
+        ) : (
+          <img
+            className="visit-icon-blowup"
+            src={process.env.PUBLIC_URL + "/visited.png"}
+            alt="a colored trail signpost"
+            onClick={() => toggleVisit(park.id)}
+          />
+        )}
+      </div>
+      <div className="park-image-container">{images}</div>
+      <div className="details-container">
+        <h2>Description:</h2>
+        <p>{park.description}</p>
+        <p>
+          <strong>Address: </strong>
+          {park.address && park.address.street}{" "}
+          {park.address && park.address.city},{" "}
+          {park.address && park.address.stateCode}{" "}
+          {park.address && park.address.zip}
+        </p>
+        <a href={park.directionsUrl}>Directions</a>
+      </div>
+    </article>
   );
 }
 
