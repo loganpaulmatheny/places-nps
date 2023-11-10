600d20c334c380523c3cd20e2dd90bae2e011dd8
Logan Matheny <loganpaulmatheny@gmail.com>
2023-11-10 11:04:34 -0500

    Add error handling for errors as well as ensure you can navigate back home when errors are clear

--- cypress/e2e/server_error.cy.js
+++ cypress/e2e/server_error.cy.js
@@ -1,4 +1,3 @@
-// cypress/e2e/server_error.cy.js
 describe("Errors", () => {
   it("A user should be notified when there's an error on the server side", () => {
     cy.intercept(
@@ -13,8 +12,54 @@ describe("Errors", () => {
       }
     ).as("getParksData");
 
-    cy.visit("http://localhost:3000").wait("@getParksData");
+    cy.visit("http://localhost:3000")
+      .wait("@getParksData")
+      .get(".error-message")
+      .should(
+        "contain",
+        "Looks like the compass might be broken, please try again later!"
+      )
+      .get(".error-image")
+      .should("have.attr", "src")
+      .and("include", `${"/compass.png"}`);
   });
+
+  it("A user should be notified when there's an error on the server side and be able to navigate home", () => {
+    cy.intercept(
+      "GET",
+      // `https://developer.nps.gov/api/v1/parks?limit=550&q=national%20park&api_key=${process.env.REACT_APP_API_KEY}`,
+      `https://developer.nps.gov/api/v1/parks?limit=550&q=national%20park&api_key=${Cypress.env(
+        "REACT_APP_API_KEY"
+      )}`,
+      {
+        statusCode: 500,
+        body: "error",
+      }
+    ).as("getParksDataError");
+
+    cy.visit("http://localhost:3000").wait("@getParksDataError");
+
+    cy.intercept(
+      "GET",
+      // `https://developer.nps.gov/api/v1/parks?limit=550&q=national%20park&api_key=${process.env.REACT_APP_API_KEY}`,
+      `https://developer.nps.gov/api/v1/parks?limit=550&q=national%20park&api_key=${Cypress.env(
+        "REACT_APP_API_KEY"
+      )}`,
+      {
+        statusCode: 200,
+        fixture: "parks.json",
+      }
+    )
+      .as("getParksDataSuccess")
+      .get(".back-button")
+      .click();
+    cy.visit("http://localhost:3000")
+      .wait("@getParksDataSuccess")
+      .get(".card:first")
+      .find("h2")
+      .should("contain", "Acadia National Park");
+  });
+
   it("A different type of server side error", () => {
     cy.intercept(
       "GET",
